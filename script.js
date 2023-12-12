ea72522487d036322decfa674b63e7406a8e9ae5
sabahussains <140971557+sabahussains@users.noreply.github.com>
2023-12-12 14:33:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿// script.js
+
+// Example: Add a simple animation to the header
+const header = document.querySelector('header');
+
+header.addEventListener('mouseover', () => {
+    header.style.backgroundColor = '#555';
+    header.style.transition = 'background-color 0.5s ease';
+});
+
+header.addEventListener('mouseout', () => {
+    header.style.backgroundColor = '#333';
+});
